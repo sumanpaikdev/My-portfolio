dd9d5a82bf824585669e9c9d358e579ba6d09501
Suman Paik <sumanpaik.dev@gmail.com>
2022-12-23 15:31:18 +0530

    updated portfolio

--- index.js
+++ index.js
@@ -11,9 +11,29 @@ let sourcetext = document.getElementById("closevw")
 
 setTimeout(function closeview() {
     sourcetext.style.display = "block"
-}, 3000)
+}, 4000)
 
 function closesource(){
     sourcetext.style.display = "none"
 
 }
+
+
+
+function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+  
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 15;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
