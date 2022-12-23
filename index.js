b15a69fb28d6947b1b8115eb8ee00853441647f7
Suman Paik <sumanpaik.dev@gmail.com>
2022-12-23 14:48:51 +0530

    updated portfolio

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// document.onreadystatechange = function() {
-//     if (document.readyState !== "complete") {
-//         document.querySelector(
-//           "body").style.visibility = "hidden";
-//         document.querySelector(
-//           "#loader").style.visibility = "visible";
-//     } else {
-//         document.querySelector(
-//           "#loader").style.display = "none";
-//         document.querySelector(
-//           "body").style.visibility = "visible";
-//     }
-// };
+let cont = document.getElementById("contactbox")
+
+function setup() {
+    cont.style.display = "block"
+} 
+function closediv() {
+    cont.style.display = "none"
+}
+
+let sourcetext = document.getElementById("closevw")
+
+setTimeout(function closeview() {
+    sourcetext.style.display = "block"
+}, 3000)
+
+function closesource(){
+    sourcetext.style.display = "none"
+
+}
